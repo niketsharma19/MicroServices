85b2081121bb74da4575b89b46a15fc4a77f8e3f
Niket Sharma <niksharm15@publicisgroupe.net>
2022-05-05 00:28:23 +0530

    Initial commit

---
+++ limits-service/src/main/java/com/niket/microservices/limitsservice/configuration/MyConfigurations.java
@@ -0,0 +1,37 @@
+package com.niket.microservices.limitsservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class MyConfigurations {
+
+	public int minimum;
+	public int maximum;
+	
+	public MyConfigurations() {
+	}
+
+	public MyConfigurations(int minimum, int maximum) {
+		this.minimum = minimum;
+		this.maximum = maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+}
---
+++ limits-service/src/main/java/com/niket/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,28 @@
+package com.niket.microservices.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niket.microservices.limitsservice.configuration.MyConfigurations;
+import com.niket.microservices.limitsservice.model.Limits;
+
+@RestController
+@RequestMapping(value = "/limits")
+public class LimitsController {
+
+	@Autowired
+	private MyConfigurations myConfigurations;
+
+	@GetMapping(value = "/getDefault")
+	public Limits getLimits() {
+		return new Limits(0, 100);
+	}
+
+	@GetMapping(value = "/getProps")
+	public Limits getLimitsProps() {
+		return new Limits(myConfigurations.minimum, myConfigurations.maximum);
+	}
+
+}
