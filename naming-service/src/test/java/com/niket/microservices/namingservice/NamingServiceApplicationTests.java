3f9933c6bba11652735cce5cead9f13cca67812b
Niket Sharma <niketsharma22@gmail.com>
2022-05-12 18:28:05 +0530

    Added Eureka naming service

---
+++ naming-service/src/test/java/com/niket/microservices/namingservice/NamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niket.microservices.namingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
